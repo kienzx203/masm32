bafded458ae22488fac191d0bc4291d45f360932
kinzx203 <97662987+kienzx203@users.noreply.github.com>
2022-10-30 23:28:29 +0700

    Update Tong_chan_le.asm
    Signed-off-by: kinzx203 <97662987+kienzx203@users.noreply.github.com>

--- Tong_chan_le.asm
+++ Tong_chan_le.asm
@@ -1,16 +1,17 @@
 .386
 .model flat, stdcall
-option casemap:none
+option casemap : none
 
-include \masm32\include\kernel32.inc 
-include \masm32\include\masm32.inc 
-includelib \masm32\lib\kernel32.lib
-includelib \masm32\lib\masm32.lib
+include	D:\masm32\include\kernel32.inc
+include	D:\masm32\include\masm32.inc
+includelib	D:\masm32\lib\kernel32.lib
+includelib	D:\masm32\lib\masm32.lib
 
 .data
 
-	num_array		dd	40 DUP (?)
-	num_output		dd	40 DUP (?)
+	num_array		db	40 DUP(?)
+	num_output		db	40 DUP(?)
+	num				db	40 DUP(?)
 	len				dd	?
 	Tong_chan		dd	0
 	Tong_le			dd	0
@@ -25,121 +26,159 @@ MAIN PROC
 	call	ATOI
 	mov		len, eax
 
-L1:	
+	L1:
+		push	40
+		push	offset num_array
+		call	StdIn
+		mov		esi, offset num_array
+		mov		edi, offset num
 
-	cmp		len, 0
-	jz		L4
-	push	40
-	push	offset num_array
-	call	StdIn
-	dec		len
-	push	offset num_array
-	call	ATOI
-	mov		ebx, 0
-	mov		ecx, 0
-	mov		ecx,eax
-	mov		ebx, 2
-	mov		edx, 0
-	div		ebx
-	cmp		edx, 0
-	jz		L3
-	jmp		L2
-
-L2:	
-	add		Tong_le,ecx
-	jmp		L1
+	L2:
+		cmp		byte ptr [esi], 0
+		je      L4
+		cmp		byte ptr [esi], 20h
+		je      L3
+		mov		eax, [esi]
+		mov		 [edi], eax
+		inc		edi
+		inc		esi
+		jmp		L2
 	
-L3: 
+	L3:
+		mov		byte ptr [edi], 0
+		mov		ebx, esi
+		push	offset num
+		call	ATOI
+		mov		esi, ebx
+		call	Sum
+		dec		len
+		inc		esi
+		mov		edi, offset num
+		jmp		L2
+	L4:
+		mov		byte ptr [edi], 0
+		mov		ebx, esi
+		push	offset num
+		call	ATOI
+		mov		esi, ebx
+		call	Sum
+		dec		len
+		cmp		len, 0
+		je		L5
+		jmp		L1
 
-	add		Tong_chan,ecx
-	jmp		L1
+	L5:
+		push	Tong_chan
+		push	offset num_output
+		call	REATOI
+		push	offset num_output
+		call	StdOut
 
-L4:
+		push	offset space
+		call	StdOut
 
-	push	Tong_chan
-	push	offset num_output
-	call	REATOI
-	push	offset num_output
-	call	StdOut
+		push	Tong_le
+		push	offset num_output
+		call	REATOI
+		push	offset num_output
+		call	StdOut
 
-	push	offset space
-	call	StdOut
+		push	0
+		call	ExitProcess
 
-	push	Tong_le
-	push	offset num_output
-	call	REATOI
-	push	offset num_output
-	call	StdOut
-
-	push	0
-	call	ExitProcess
-
-	
 MAIN ENDP
+
+
+Sum PROC
+	push	ebp
+	mov		ebp,esp
+	L1:
+		mov		ebx, 0
+		mov		ecx, 0
+		mov		ecx, eax
+		mov		ebx, 2
+		mov		edx, 0
+		div		ebx
+		cmp		edx, 0
+		jz		L3
+		jmp		L2
+
+	L2 :
+		add		Tong_le, ecx
+		pop		ebp
+		ret
+
+	L3 :
+
+		add		Tong_chan, ecx
+		pop		ebp
+		ret
+
+Sum ENDP
 
 ATOI PROC
 	push	ebp
 	mov		ebp, esp
 	push	ebx
-	mov		ebx, [ebp+08h]
-	xor		esi, esi								
+	mov		ebx, [ebp + 8h]
+	xor		esi, esi
 	xor		eax, eax
 	mov		ecx, 10
 
-L1:
-	xor		edx, edx
-	mov		dl, byte ptr [ebx+esi]				
-	cmp		dl, 0								
-	jz		L2
-	sub 	edx, 30h							
-	add		eax, edx							
-	mul		ecx									
-	inc		esi									
-	jmp		L1
+	L1:
+		xor		edx, edx
+		mov		dl, byte ptr[ebx + esi]
+		cmp		dl, 0
+		jz		L2
+		sub 	edx, 30h
+		add		eax, edx
+		mul		ecx
+		inc		esi
+		jmp		L1
 
-L2:
-	div		ecx
-	pop		ebx
-	pop		ebp
-	ret		4
+	L2 :
+		div		ecx
+		pop		ebx
+		pop		ebp
+		ret		4
 
 ATOI ENDP
 
 REATOI PROC
 
-    push    ebp
-    mov     ebp, esp
+	push    ebp
+	mov     ebp, esp
 	xor		eax, eax
 	xor		ebx, ebx
-    mov     eax, [ebp + 0Ch]						
-    mov     ebx, [ebp + 08h]						
-    xor     esi, esi 
-    mov     ecx, 10
-    push    3Ah										
+	mov     eax, [ebp + 0Ch]
+	mov     ebx, [ebp + 8h]
+	xor		esi, esi
+	mov     ecx, 10
+	push    3Ah
 
-L1:
-    xor     edx, edx
-    div     ecx									
-    or      edx, 30h						
-    push    edx									
-    cmp     eax, 0						
-    jz      L2
-    jmp     L1
+	L1 :
+		xor		edx, edx
+		div     ecx
+		or		edx, 30h
+		push    edx
+		cmp     eax, 0
+		jz      L2
+		jmp     L1
 
-L2:
+	L2 :
 
-    pop     edx
-    cmp     dl, 3Ah								
-    jz      L3						
-    mov     byte ptr [ebx + esi], dl			
-    inc     esi
-    jmp     L2
+		pop     edx
+		cmp     dl, 3Ah
+		jz      L3
+		mov     byte ptr[ebx + esi], dl
+		inc     esi
+		jmp     L2
 
-L3:
+	L3 :
 
-    mov     byte ptr [ebx + esi], 0
-    pop     ebp
-    ret     8
+		mov     byte ptr[ebx + esi], 0
+		pop     ebp
+		ret     8
 
 REATOI ENDP
 END MAIN
