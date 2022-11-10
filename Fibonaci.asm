2012ab1e378cdda9e98e7abb3bc7eaba0aa124bd
kinzx203 <97662987+kienzx203@users.noreply.github.com>
2022-11-10 13:53:26 +0700

    Update Fibonaci.asm
    Signed-off-by: kinzx203 <97662987+kienzx203@users.noreply.github.com>

--- Fibonaci.asm
+++ Fibonaci.asm
@@ -1,5 +1,5 @@
 .386
-.model flat,stdcall
+.model flat, stdcall
 option casemap:none
 
 include \masm32\include\kernel32.inc 
@@ -8,200 +8,302 @@ includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\masm32.lib
 
 .data
-	
-	input		dd		30 DUP (?)
-	output		dd		30 DUP (?)
-	num		dd		0
-	num2		dd		0
-	num3		dd		1
-	nline		db		20h, 20h, 0
+	n		db 100 dup(?)
+	num_n	dd 0
+	mmry	db 0
+	print0	db '0', 20h, 0
+	print1	db '1', 20h, 0
+	s_f1	db '1', 99 dup(0), 0
+	s_f2	db 100 dup(0), 0
+	s_fn	db 101 dup(0)
+	fn_fix	db 101 dup(0)
+	bspace	db 20h, 0
+
 .code
-MAIN PROC
-	push	30
-	push	offset input
+main PROC
+	push	offset s_f1
+	call	reverse
+
+	push	100
+	push	offset n
 	call	StdIn
-	push	offset input
-	call	ATOI
- 	mov	num, eax
-	cmp	num ,3
-	jge	L1
-	call	FIBONACI
 
-L1:
-	mov	eax, 0
-	push	eax
-	push	offset output
-	call	REATOI
-	push	offset output
-	call	StdOut
-	push	offset nline
-	call	StdOut
-	mov	eax, 1
-	push	eax
-	push	offset output
-	call	REATOI
-	push	offset output
-	call	StdOut
-	push	offset nline
-	call	StdOut
-	call	FIBONACI
-	push	eax
-	push	offset output
-	call	REATOI
-	push	offset output
-	call	StdOut
-	push	0
-	call	ExitProcess
+	push	offset n
+	call	atoi
+	mov		num_n, eax
 
-MAIN ENDP
+	cmp		num_n, 0
+	jz		print01
+	cmp		num_n, 1
+	jz		print01
+	cmp		num_n, 2
+	je		print03
+	jmp		print_fibo
 
-ATOI PROC
-	push	ebp
-	mov	ebp, esp
+	print01:
+		push	offset print0
+		call	StdOut
+		jmp		exit
+
+	print02 :
+		push	offset print1
+		call	StdOut
+		jmp		exit
+	print03 :
+		push	offset print0
+		call	StdOut
+		push	offset print1
+		call	StdOut
+		jmp		exit
+
+	print_fibo:
+		push	offset print0
+		call	StdOut
+
+		push	offset print1
+		call	StdOut
+
+		mov		ecx, 2
+		mov		eax, num_n
+		sub		eax, ecx
+		mov		num_n, eax
+		mov		eax, 0
+		jmp		find_fibo
+
+	find_fibo:
+		mov		ecx, num_n
+		cmp		num_n, 0
+		je		exit
+
+		push	offset s_f1
+		push	offset s_f2
+		push	offset s_fn
+		call	ADD_TWO
+
+		push	offset s_fn
+		call	reverse
+		push	offset s_fn
+		call	StdOut
+		push	offset s_fn
+		call	reverse
+
+		push	offset s_f1
+		push	offset s_f2
+		call	copy
+		push	offset s_fn
+		push	offset s_f1
+		call	copy
+
+		push	offset bspace
+		call	StdOut
+		mov		ecx, num_n
+		dec		ecx
+		mov		num_n, ecx
+		jmp		find_fibo
+
+	exit:
+		push	0
+		call	ExitProcess
+
+
+main ENDP
+
+ADD_TWO PROC
+	
+		push	ebp
+		mov		ebp, esp
+		mov		edx, [ebp + 8];		dia chi fn
+		mov		edi, [ebp + 12];	dia chi f2
+		mov		esi, [ebp + 16];	dia chi f1
+		mov		eax, 0
+
+	L1:
+		cmp		byte ptr [esi], 0
+		jz		exit
+		mov		al, byte ptr[edi]
+		mov		ah, byte ptr[esi]
+		sub		ah, 30h
+		cmp		al, 0h
+		jz		L3
+		sub		al, 30h
+		add		al, ah
+		add		al, [mmry]
+		cmp		al, 0Ah
+		jae		L2
+		mov		[mmry], 0
+		add		al, 30h
+		mov		[edx], al
+		inc		edx
+		inc		edi
+		inc		esi
+		jmp		L1
+
+	L2:
+		sub		al, 0Ah
+		mov		[mmry], 1
+		add		al, 30h
+		mov		[edx], al
+		inc		edx
+		inc		esi
+		inc		edi
+		jmp		L1
+
+	L3:
+		add		al, ah
+		add		al, [mmry]
+		cmp		al, 0Ah
+		jae		L2
+		mov		[mmry], 0
+		add		al, 30h
+		mov		[edx], al
+		inc		edx
+		inc		edi
+		inc		esi
+		jmp		L1
+
+	exit:
+		cmp		[mmry], 1
+		jz		exit2
+		pop ebp
+		ret 12
+	exit2: 
+		mov		byte ptr [edx], 31h
+		mov		[mmry], 0
+		pop		ebp
+		ret		12
+			
+
+ADD_TWO ENDP
+
+copy PROC
+	push	ebp	
+	mov		ebp, esp
+	push	eax
 	push	ebx
-	mov	ebx, [ebp+08h]
-	xor	esi, esi								
-	xor	eax, eax
-	mov	ecx, 10
+	mov		eax, [ebp+0Ch]			;chuoi duoc cp
+	mov		ebx, [ebp+08h]			;chuoi cp
+	xor		esi, esi
+	xor		edi, edi
 
-L1:
-	xor	edx, edx
-	mov	dl, byte ptr [ebx+esi]				
-	cmp	dl, 0								
-	jz	L2
-	sub 	edx, 30h							
-	add	eax, edx							
-	mul	ecx									
-	inc	esi									
-	jmp	L1
+	for_copy:
+		xor		edx, edx
+		mov		dl, byte ptr [eax+esi]
+		cmp		dl, 0
+		jz		done_copy
+		mov		byte ptr [ebx+esi], dl
+		inc		esi
+		jmp		for_copy
 
-L2:
-	div	ecx
-	pop	ebx
-	pop	ebp
-	ret	4
+	done_copy:
+		mov		byte ptr [ebx+esi], 0
+		pop		ebx
+		pop		eax
+		pop		ebp
+		ret		8
 
-ATOI ENDP
+copy ENDP
 
-REATOI PROC
-
-    push    ebp
-    mov     ebp, esp
-    xor     eax, eax
-    xor	    ebx, ebx
-    mov     eax, [ebp + 0Ch]						
-    mov     ebx, [ebp + 08h]						
-    xor     esi, esi 
-    mov     ecx, 10
-    push    3Ah										
-
-L1:
-    xor     edx, edx
-    div     ecx									
-    or      edx, 30h						
-    push    edx									
-    cmp     eax, 0						
-    jz      L2
-    jmp     L1
-
-L2:
-
-    pop     edx
-    cmp     dl, 3Ah								
-    jz      L3						
-    mov     byte ptr [ebx + esi], dl			
-    inc     esi
-    jmp     L2
-
-L3:
-
-    mov     byte ptr [ebx + esi], 0
-	pop     ebp
-    ret     8
-
-REATOI ENDP
-
-FIBONACI PROC
+reverse PROC
 	push	ebp
-	mov	ebp, esp
-	mov	ecx, 0
-	mov	ecx, num
-	xor	eax, eax
-	xor	ebx, ebx
-	jmp	L1
-
-L3:	
-	mov	eax, 0
+	mov		ebp, esp
 	push	eax
-	push	offset output
-	call	REATOI
-	push	offset output
-	call	StdOut
-	push	0
-	call	ExitProcess
+	mov		eax, [ebp+08h]
+	xor		esi, esi
+	xor		edi, edi
+	push	0h
 
-L1:
-	
-	cmp	ecx, 0
-	je	L3
-	cmp	ecx, 1
-	je	L3
-	cmp	ecx, 2
-	je	L6
-	jmp	L4
+	for_re:
+		xor		edx, edx
+		mov		dl, byte ptr [eax+esi]
+		cmp		dl, 0
+		jz		pop_re
+		push	edx
+		inc		esi
+		jmp		for_re
 
-L4:
-	xor	eax, eax
-	xor	ebx, ebx
-	mov	eax, num2
-	mov	ebx, num3
-	mov	num2,ebx
-	add	eax, ebx
-	mov	num3,eax
-	cmp	ecx, 3
-	je	L5
-	jmp	L7
-L5:
-	xor	eax, eax
-	mov	eax, num3
-	pop	ebp
-	ret	4
+	pop_re:
+		xor		edx, edx
+		pop		edx
+		cmp		dl, 0h
+		jz		break_re
+		mov		byte ptr [eax+edi], dl
+		inc		edi
+		jmp		pop_re
 
-L6:
-	
-	mov		eax, 0
-	push	eax
-	push	offset output
-	call	REATOI
-	push	offset output
-	call	StdOut
-	push	offset nline
-	call	StdOut
-	mov		eax, 1
-	push	eax
-	push	offset output
-	call	REATOI
-	push	offset output
-	call	StdOut
-	push	0
-	call	ExitProcess	
+	break_re:
+		mov		byte ptr [eax+edi], 0
+		pop		eax
+		pop		ebp	
+		ret		4
 
-L7:
+reverse ENDP
 
-	mov		ebx, ecx
+atoi PROC
+	push	ebp
+	mov		ebp, esp
 	push	ebx
-	push	eax
-	push	offset output
-	call	REATOI
-	pop		ebx
-	push	offset output
-	call	StdOut
-	push	offset nline
-	call	StdOut
-	mov	ecx, ebx
-	dec	ecx
-	jmp	L4
-	
-FIBONACI ENDP
-END MAIN
+	mov		ebx, [ebp+08h]
+	xor		esi, esi
+	xor		eax, eax
+	mov		edi, 10
 
+	for_atoi:
+		xor		edx, edx
+		mov		dl, byte ptr [ebx+esi]
+		cmp		dl, 0
+		jz		break_atoi
+		sub		edx, 30h
+		add		eax, edx
+		mul		edi
+		inc		esi
+		jmp		for_atoi
+
+	break_atoi:
+		mov		byte ptr [ebx+esi], 0
+		div		edi
+		pop		ebx
+		pop		ebp
+		ret		4
+
+atoi ENDP
+
+itoa PROC
+	push	ebp
+	mov		ebp, esp
+	push	eax
+	push	ebx
+	mov		eax, [ebp+0Ch]		;number
+	mov		ebx, [ebp+08h]		;string
+	xor		esi, esi
+	mov		edi, 10
+	push	69h
+
+	for_itoa:
+		xor		edx, edx
+		div		edi
+		xor		edx, 30h
+		push	edx
+		cmp		eax, 0
+		jz		pop_itoa
+		jmp		for_itoa
+
+	pop_itoa:
+		xor		edx, edx
+		pop		edx
+		cmp		dl, 69h
+		jz		break_itoa
+		mov		byte ptr [ebx+esi], dl
+		inc		esi
+		jmp		pop_itoa
+	
+	break_itoa:
+		mov		byte ptr [ebx+esi], 0
+;		div		edi
+		xor		edi, edi
+		pop		ebx
+		pop		eax
+		pop		ebp
+		ret		8
+
+itoa ENDP
+END main
